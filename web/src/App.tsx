57f77fded05247599cc6ecc8b2808bf29a63c88e
Thijs W <mawijnen2@student.avans.nl>
2019-12-06 01:43:41 +0100

    changed standard template

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/face.png';
 import './App.css';
 
 const App: React.FC = () => {
@@ -8,16 +8,9 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Puremonk.net
+          <div>A work in progress</div>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
